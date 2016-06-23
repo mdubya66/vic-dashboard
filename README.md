9c0fbe523b3064e7f0ace09a120ab0be105f3255
Ken Platz <kplatz@vmware.com>
2016-06-23 13:15:07 -0500

    Prototype completed

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # vic-dashboard
+
+This is a very early prototype of the VIC dashboard, built on Go.  It has been 
+tested against Go-1.6.2, but should run on any current version of Go.  
+
+Prerequisites:  In order to run this, you must have a OAuth2 token from Github.
+This can either be set as an environment variable, VIC_DASHBOARD_TOKEN, or it can be
+stored in a file and specified with the --tokenfile parameter.  The options available
+are:
+
+    --user=<user>      The github user to pull bugs from.  Default="vmware"
+    --repo=<repo>      The github repo to pull bugs from.  Default="vic"
+    --port=<port>      The (local) port to listen to.      Default=8888
+    --tokenfile=<file> Filename containing OAuth2 token.   No default
+
+The application will pull all issues from the vmware/vic repository and display them
+on a web page at http://localhost:port
+
+This is very much a work in progress.
+
+
--- src/vic-dashboard.go
+++ src/vic-dashboard.go
@@ -20,13 +20,36 @@ import (
     "io"
     "os"
     "sort"
+    "encoding/base64"
     "github.com/google/go-github/github"
     "golang.org/x/oauth2"
-    "net/html"
+    "golang.org/x/image/colornames"
+    "net/http"
+
+    "github.com/gonum/plot"
+    "github.com/gonum/plot/plotter"
+    "github.com/gonum/plot/vg"
 )
+
+type XYs plotter.XYs
 
 type TokenSource struct {
     AccessToken string
+}
+type XY struct { X, Y float64 }
+func (slice XYs) Len() int {
+    return len(slice)
+}
+
+func (slice XYs) Less(i, j int) bool {
+    return slice[i].X < slice[j].X
+}
+func (slice XYs) Swap(i, j int) {
+    slice[i].X, slice[j].X = slice[j].X, slice[i].X
+    slice[i].Y, slice[j].Y = slice[j].Y, slice[i].Y
+}
+func (slice XYs)  XY(i int)( x,y float64 ) {
+    return slice[i].X, slice[i].Y
 }
 
 func (t *TokenSource) Token() (*oauth2.Token, error) {
@@ -36,13 +59,16 @@ func (t *TokenSource) Token() (*oauth2.Token, error) {
     return token, nil
 }
 
+
+var issues map[int]github.Issue
+
 //
 // Connect to github and iteratively pull all issues from the
 // repository
 //
 func getAllIssues( user, repo *string, 
                    client *github.Client ) map[int]github.Issue {
-    issues := make(map[int]github.Issue)
+    issues = make(map[int]github.Issue)
 
     ilo := new(github.IssueListByRepoOptions)
     ilo.State = "all"
@@ -97,6 +123,179 @@ func printAllIssues( issues map[int]github.Issue ) {
         } 
     }
     fmt.Println("")
+}
+
+//
+// Generate a graph of the bugs in terms of open, closed, and total
+// Write these to a the http.ResponseWriter
+//
+func graphBugs(w http.ResponseWriter, r *http.Request ) {
+    keys := []int{}
+    for k, _ := range issues {
+        keys = append( keys, k )
+    }
+    sort.Ints(keys)
+
+    allBugs := XYs{}
+    closedBugs := XYs{}
+    openBugs := XYs{}
+    
+    point := XY{ 0.0, 1 }
+
+    // Create data points for when each bug is open or closed
+    for _, i := range keys {
+        iss := issues[i]
+
+        isBug := false
+        for _, l := range iss.Labels {
+            if *l.Name == "kind/bug" {
+                isBug = true
+            }
+        }
+        if isBug == false {
+            continue
+        }
+/*
+        fmt.Fprint( w, "Issue # ", *iss.Number, "(", *iss.State, ") Assigned to: " )
+        if iss.Assignee == nil {
+            fmt.Fprint( w, "<no one>" )
+        } else {
+            fmt.Fprint( w, *iss.Assignee.Login )
+        }
+
+        for _, label := range iss.Labels {
+            fmt.Fprint( w, " <", *label.Name, ">" )
+        }
+        fmt.Fprint( w, " Created: ", *iss.CreatedAt, " Closed: " )
+        if ( iss.ClosedAt != nil ) {
+            fmt.Fprintln( w, *iss.ClosedAt )
+        } else {
+            fmt.Fprintln( w, "<open>" )
+        } 
+*/
+
+        t := *iss.CreatedAt
+        point.X = float64(t.Unix())
+        allBugs = append( allBugs, point )
+        if iss.ClosedAt != nil {
+            t = *iss.ClosedAt
+            point.X = float64( t.Unix() )
+            closedBugs = append( closedBugs, point )
+        }
+    }
+
+    // Sort bugs by time.
+    // Technically allBugs should already be sorted by time,
+    // but the closed date is only partially sorted
+    sort.Sort( allBugs )
+    sort.Sort( closedBugs )
+    for i:=0; i<len(allBugs); i++ {
+        allBugs[i].Y = float64(i+1)
+    }
+    for i:=0; i<len(closedBugs); i++ {
+        closedBugs[i].Y = float64(i+1)
+    }
+    ab := 0  // count of all bugs
+    cb := 0  // count of closed bugs
+    ob := 0  // count of open bugs
+
+    // Now let's create our X,Y points to count the open, closed, and all bugs
+    for ab<len(allBugs) && cb<len(closedBugs) {
+        if allBugs[ab].X < closedBugs[cb].X {
+            ob++
+            point = XY{ allBugs[ab].X, float64(ob) }
+            ab++
+        } else {
+            ob--
+            point = XY{ closedBugs[cb].X, float64(ob) }
+            cb++
+        }
+        openBugs = append( openBugs, point )
+    }
+    if ab < len(allBugs) {
+        for ab < len(allBugs) {
+            ob++
+            point = XY{ allBugs[ab].X, float64(ob) }
+            openBugs = append( openBugs, point )
+            ab++
+        }
+    } else {
+        for cb < len(closedBugs) {
+            ob--
+            point = XY{ closedBugs[ab].X, float64(ob) }
+            openBugs = append( openBugs, point )
+            cb++
+        }
+    }
+
+    // Now to plot the data
+    p, err := plot.New()
+    if err != nil {
+        panic(err)
+    }
+    p.Title.Text = "vmware/vic Bug History"
+    p.Y.Label.Text = "Number of Bugs"
+    p.X.Tick.Marker = plot.UnixTimeTicks{Format: "2006-01-02"}
+    p.Add(plotter.NewGrid())
+    p.Legend.Top, p.Legend.Left = true, true
+
+    line, err := plotter.NewLine(allBugs)
+    if err != nil {
+        panic(err)
+    }
+    line.Color = colornames.Blue
+    p.Add( line )
+    p.Legend.Add( "All Bugs", line )
+
+    line, err = plotter.NewLine(closedBugs)
+    if err != nil {
+        panic(err)
+    }
+
+    line.Color = colornames.Green
+    p.Add( line )
+    p.Legend.Add( "Closed Bugs", line )
+
+    line, err = plotter.NewLine(openBugs)
+    if err != nil {
+        panic(err)
+    }
+
+    line.Color = colornames.Red
+    p.Add( line )
+    p.Legend.Add( "Open Bugs", line )
+
+    // Temporary spot to save to a PNG file
+    err = p.Save(6*vg.Inch, 6*vg.Inch, "test.png")
+    if err != nil {
+        panic(err)
+    }
+
+    // Write a page to the http.ResponseWriter
+    wr, err := p.WriterTo( 6 * vg.Inch, 6 * vg.Inch, "png" )
+    if err != nil {
+        panic(err)
+    }
+    
+    // Encode it as base64
+    var b bytes.Buffer
+    b64w := base64.NewEncoder( base64.StdEncoding, &b )
+    wr.WriteTo( w )
+    b64w.Close()
+    str := string( b.Bytes() )
+
+    fmt.Fprint( w,
+        `<!DOCTYPE HTML>
+         <html>
+         <head>
+         <title>VIC Bug History</title>
+         </head>
+
+         <body>
+         <img alt="VIC Bug History Image" src="data:image/png;charset=utf8;base64, ` )
+    fmt.Fprint( w, str )
+    fmt.Fprintln( w, `" />` )
+    fmt.Fprintln( w, `<body></html` )
 }
 
 func main() {
@@ -154,4 +353,8 @@ func main() {
 
     printAllIssues( issues )
 
+    http.HandleFunc( "/", graphBugs )
+    serverPort := fmt.Sprintf( ":%d", *portPtr )
+    fmt.Println( "Listening on port ", serverPort )
+    http.ListenAndServe( serverPort, nil )
 }
